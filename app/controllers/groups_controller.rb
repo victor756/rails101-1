23553960cb7f5f0589f67166841b5e88e0679be5
郭淼 <victorhuo756@gmail.com>
2017-03-30 06:54:20 +0800

    add order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,9 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   end
 
   def show
-   @group = Group.find(params[:id])
-    @posts = @group.posts
- end
+    @group = Group.find(params[:id])
+    @posts = @group.posts.order("created_at DESC")
+  end
  def edit
  end
 
